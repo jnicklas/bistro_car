aece73e808fccb86d027faf28695fce0eefbd987
Jonas Nicklas <jonas.nicklas@gmail.com>
2010-01-03 21:51:04 +0100

    Set proper content type header for JS

--- app/controllers/bistro_car/bundle_controller.rb
+++ app/controllers/bistro_car/bundle_controller.rb
@@ -2,6 +2,7 @@ class BistroCar::BundleController < ActionController::Base
   caches_page :show
 
   def show
+    headers['Content-Type'] = "application/javascript"
     render :text => BistroCar::Bundle.new(params[:bundle]).to_javascript
   end
 end
