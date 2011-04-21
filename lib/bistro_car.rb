db12db6f1539c21d055c46758a1a4b8ecf56de82
lennart <me@lmaa.name>
2011-04-21 15:07:13 +0200

    Raise and report if CoffeeScript compile fails

--- lib/bistro_car.rb
+++ lib/bistro_car.rb
@@ -11,17 +11,26 @@ module BistroCar
       engine_name :bistro_car
     end
   end
-  
+
   class << self
     def compile(source)
+      # Return fast on empty source!
+      return "" if source.blank?
       file = Tempfile.new('script.coffee')
+      jsfile = "#{file.path}.js"
       file.write(source)
       file.close
-      %x(coffee -p #{file.path})
+      # Check for generated content, raise on empty output…
+      if (content = %x(coffee -p #{file.path})).blank?
+        raise CoffeeScriptCompileError.new(%x(coffee -p #{file.path} 2>&1))
+      else
+        content
+      end
     end
-  
+
     attr_accessor :mode, :minify
   end
+  class CoffeeScriptCompileError < RuntimeError; end
 end
 
 BistroCar.mode = :bundled
